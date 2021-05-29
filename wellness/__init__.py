4644dcbb087966e7874fecfde6d9d4b89c1be6d4
csavella <csavella@pdx.edu>
2021-05-28 19:29:18 -0700

    putting all init in main

--- wellness/__init__.py
+++ wellness/__init__.py
@@ -6,23 +6,20 @@ from models import User
 from routes.auth import auth
 from routes.main import main
 
-def create_app(config_file = 'settings.py'):
-    app = Flask(__name__)
-    app.config.from_pyfile(config_file)
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(user_id)
+config_file = 'settings.py'
+app = Flask(__name__)
+app.config.from_pyfile(config_file)
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
-    app.register_blueprint(main)
-    app.register_blueprint(auth)
+app.register_blueprint(main)
+app.register_blueprint(auth)
 
-    app.cli.add_command(create_tables)
-
-    return app
+app.cli.add_command(create_tables)
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(threaded=True, port=5000)
