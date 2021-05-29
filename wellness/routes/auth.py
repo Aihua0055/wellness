02c8183bf5dbf9e4866d2cf3c3dc2af447f383a1
csavella <csavella@pdx.edu>
2021-05-28 19:22:42 -0700

    remove user login functionality

--- wellness/routes/auth.py
+++ wellness/routes/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 from werkzeug.security import check_password_hash
 #from wellness.extensions import db
-from wellness.models import User
+#from wellness.models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -12,12 +12,12 @@ def register():
         name = request.form['name']
         unhashed_password = request.form['password']
 
-        user = User(
-            name=name, 
-            unhashed_password=unhashed_password,
-            admin=False,  
-            expert=False
-        )
+        #user = User(
+        #    name=name, 
+        #    unhashed_password=unhashed_password,
+        #    admin=False,  
+        #    expert=False
+        #)
 
         #db.session.add(user)
         #db.session.commit()
@@ -44,16 +44,16 @@ def login():
         name = request.form['name']
         password = request.form['password']
 
-        user = User.query.filter_by(name=name).first()
+        # user = User.query.filter_by(name=name).first()
 
         error_message = ''
 
-        if not user or not check_password_hash(user.password, password):
-            error_message = 'Could not login. Please check and try again.'
+        # if not user or not check_password_hash(user.password, password):
+        #     error_message = 'Could not login. Please check and try again.'
 
-        if not error_message:
-            login_user(user)
-            return redirect(url_for('main.index'))
+        # if not error_message:
+        #     login_user(user)
+        #     return redirect(url_for('main.index'))
     kwargs = {
         'title': 'Login',
         'jumbotron': {
