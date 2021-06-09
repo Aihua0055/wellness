6ae067d2710e3d788ae00a84c06d14ed868d0789
csavella <csavella@pdx.edu>
2021-06-08 22:23:01 -0700

    Changing to percentage

--- wellness/routes/evidence.py
+++ wellness/routes/evidence.py
@@ -104,15 +104,15 @@ def default_activity_page():
             "to potentially harmful substances can help keep you healthier.",
             "Our surroundings and how they influence our choices form the foundation for a healthy " +
             "lifestyle.",
-            "It’s estimated that healthcare services account for just 10&#37; of longevity, while " +
-            "social and environmental factors account for twice that at 20&#37;, genetics 30&#37;, and " +
-            "individual behaviors an estimated 40&#37; (Schroeder, NEJM, 2007).",
+            "It’s estimated that healthcare services account for just 10% of longevity, while " +
+            "social and environmental factors account for twice that at 20%, genetics 30%, and " +
+            "individual behaviors an estimated 40% (Schroeder, NEJM, 2007).",
             "Being in a high-stress environment such as on a highly-trafficked street will cause the " +
             "brain to signal production of cortisol by the adrenal glands. Elevated cortisol interferes " +
             "with learning and memory, weakens immune function and bone density, and increases weight " +
             "gain, blood pressure and heart disease(Franke, Children, 2014).",
             "Memory and attention spans also benefit from time in nature, with University of Michigan " +
-            "psychology researchers pointing to a 20&#37; improvement after spending an hour " +
+            "psychology researchers pointing to a 20% improvement after spending an hour " +
             "interacting with nature",
             ]
 
